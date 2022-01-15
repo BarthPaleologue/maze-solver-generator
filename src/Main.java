e3ff237d4709602f6eb565dfb0ec61df071dc44b
Barthélemy Paléologue <31370477+BarthPaleologue@users.noreply.github.com>
2022-01-15 19:45:15 +0100

    Removing useless imports

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,10 @@
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
-import dijkstra.Dijkstra;
-import dijkstra.Previous;
-import dijkstra.VertexInterface;
 import maze.Maze;
 import maze.MazeReadingException;
 import ui.Window;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
+	public static void main(String[] args) {
 		Maze maze = new Maze();
 		
 		Window w = new Window("PROJEEEET");
